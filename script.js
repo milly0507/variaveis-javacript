4edb2c9ca96406d71842175b8276e6c1f1d43968
milly0507 <thaemilly.almeida@escola.pr.gov.br>
2026-09-08 15:06:40 +0000

    variaveis

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome = "Felipe"
+let idade = 19;
+let cidade = "Londrina";
+let altura = 1.84;
+
+console.lg("Nome: "+nome);
+console.lg("Idade: "+idade);
+console.lg("Altura: "+altura)
+console.lg("Cidade:: "+altura);
+
